74827fe21537f7e64124a555f033ba2c20b94c29
Sebastian Larsson <sebastian.larsson98@gmail.com>
2022-11-03 22:41:04 +0100

    Add hjkl navigation for nvim

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,6 +2,11 @@ local fn = vim.fn
 local map = vim.api.nvim_set_keymap
 vim.g.mapleader = " "
 
+map('n', '<Leader>j', '<C-w><Down>', {noremap=true, silent=true})
+map('n', '<Leader>k', '<C-w><Up>', {noremap=true, silent=true})
+map('n', '<Leader>h', '<C-w><Left>', {noremap=true, silent=true})
+map('n', '<Leader>l', '<C-w><Right>', {noremap=true, silent=true})
+
 map('n', '<Leader>ff', ':Telescope find_files<Cr>', {noremap=true, silent=true})
 map('n', '<Leader>gr', ':Telescope live_grep<Cr>', {noremap=true, silent=true})
 map('n', '<Leader>man', ':Telescope man_pages<Cr>', {noremap=true, silent=true})
