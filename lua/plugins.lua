8d26f9c902b8cd9d52a8390adceb78d773fd4a52
Sebastian Larsson <sebastian.larsson98@gmail.com>
2022-12-02 22:45:22 +0100

    Add spotify for nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -81,6 +81,25 @@ return packer.startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
+  -- Spotify
+  use {
+    'KadoBOT/nvim-spotify',
+    requires = 'nvim-telescope/telescope.nvim',
+    config = function()
+      local spotify = require'nvim-spotify'
+        spotify.setup {
+          -- default opts
+          status = {
+            update_interval = 10000, -- the interval (ms) to check for what's currently playing
+            format = '%s %t by %a' -- spotify-tui --format argument
+          }
+        }
+      end,
+    run = 'make'
+  }
+
+  use { 'MunifTanjim/nui.nvim' }
+
   -- luarocks
   use_rocks { 'luasocket' }
   use_rocks { 'json-lua' }
