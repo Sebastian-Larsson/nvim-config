c3291915497fb0077af31a86bc5ba0c503de8fe9
Sebastian Larsson <sebastian.larsson98@gmail.com>
2022-12-02 22:43:55 +0100

    Add shortcuts to conf, tmux, and gerrit-review

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,6 +7,10 @@ map('n', '<Leader>k', '<C-w><Up>', {noremap=true, silent=true})
 map('n', '<Leader>h', '<C-w><Left>', {noremap=true, silent=true})
 map('n', '<Leader>l', '<C-w><Right>', {noremap=true, silent=true})
 
+map('n', '<Leader>conf', ':e ~/.config/nvim/lua/plugins.lua<Cr>', {noremap=true, silent=true})
+map('n', '<Leader>plug', ':e ~/neovim-plugins/gerrit-review.nvim/<Cr>', {noremap=true, silent=true})
+map('n', '<Leader>tmux', ':e ~/.tmux.conf<Cr>', {noremap=true, silent=true})
+
 map('n', '<Leader>ff', ':Telescope find_files<Cr>', {noremap=true, silent=true})
 map('n', '<Leader>gr', ':Telescope live_grep<Cr>', {noremap=true, silent=true})
 map('n', '<Leader>man', ':Telescope man_pages<Cr>', {noremap=true, silent=true})
